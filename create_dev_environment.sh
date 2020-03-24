3b692e8f8f839c78f3237e78a808816f60ab1a79
Paolo Genissel <paolo@getaround.com>
2020-03-24 12:03:13 +0100

    Added MLFLOW_TRACKING_URI environment variable in create_dev_environment.sh

--- create_dev_environment.sh
+++ create_dev_environment.sh
@@ -6,6 +6,7 @@ mlflow server \
   --default-artifact-root file://$BUILD_DIRECTORY \
   --host 0.0.0.0 &
 sleep 2
+export MLFLOW_TRACKING_URI=http://localhost:5000
 mlflow experiments create -n test_sklearn
 python -m examples.training.sklearn
 mlflow experiments create -n test_pytorch
