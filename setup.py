99dfe71d39c411e0461db7b1e32b04df70d2094d
Paolo Genissel <paolo@getaround.com>
2020-03-25 17:31:07 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="mlserve",
     version="0.1",
-    description="mlserve is a machine learning serving tool",
-    url="http://github.com/gfalcone/mlserve",
     author="gfalcone",
     author_email="gfalcone@github.com",
-    license="Apache",
+    description="mlserve is a machine learning serving tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/gfalcone/mlserve",
     packages=find_packages(),
+    license="Apache",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     zip_safe=False,
 )
