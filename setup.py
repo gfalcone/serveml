024988ef063f7f75da572e78edfd34c444eff58b
Paolo Genissel <paolo@getaround.com>
2020-03-26 10:12:30 +0100

    Blackify setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     required = f.read().splitlines()
 
 setup(
     name="serveml",
-    version="0.2",
+    version="0.2.1",
     author="gfalcone",
     author_email="gfalcone@github.com",
     description="serveml is a machine learning serving tool",
@@ -23,6 +23,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     zip_safe=False,
 )
